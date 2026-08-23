ebe3de9e173b6b938256b992ebd789b5371f92c6
Divyansh Singh <24dcs032@lnmiit.ac.in>
2026-08-23 15:15:50 +0530

    created api for authentication

---
+++ server/routes/authRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getMe, loginUser, registerUser } from "../controllers/authcontroller.js";
+import { protect } from "../middleware/auth.js";
+
+const authRouter = Router()
+
+authRouter.post("/register", registerUser)
+authRouter.post("/login", loginUser)
+authRouter.get("/me", protect, getMe)
+
+export default authRouter;
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+
 const app = express();
 
 await connectDB()
@@ -10,11 +12,22 @@ await connectDB()
 app.use(cors())
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use("/api/auth", authRouter)
+
+// Global Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
+    console.error("Unhandle Error:", err);
+    res.status(500).json({
+        message: err.message || "Internal Server Error",
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
