99b04bf31c2da57561e14edf0354bc421019d171
divbytes-prog <24dcs032@lnmiit.ac.in>
2026-09-21 00:07:41 +0530

    Fix restaurant availability route matching

--- server/routes/RestaurantRoutes.ts
+++ server/routes/RestaurantRoutes.ts
@@ -5,7 +5,7 @@ const restaurantRouter = Router();
 
 restaurantRouter.get('/', getRestaurants);
 restaurantRouter.get('/featured', getFeaturedRestaurants);
-restaurantRouter.get('/:slug', getRestaurantBySlug);
 restaurantRouter.get('/:id/availability', getRestaurantAvailability);
+restaurantRouter.get('/:slug', getRestaurantBySlug);
 
 export default restaurantRouter;
